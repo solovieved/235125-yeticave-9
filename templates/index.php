972ff6139d87eb152318673a4132fdf6c28b0458
Admin <soloviev.ed16@gmail.com>
2019-04-22 01:48:40 +0300

    Создание схемы БД

--- templates/index.php
+++ templates/index.php
@@ -3,11 +3,12 @@
     <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
     <ul class="promo__list">
         <!--список из массива категорий-->
-        <?php foreach ($categories as $key => $value): ?>
-        <li class="promo__item promo__item--<?= $value['class']; ?>">
-            <a class="promo__link" href="pages/all-lots.html"><?= $value['name']; ?></a>
-        </li>
+        <?php foreach ($categories as $key => $value) : ?>
+            <li class="promo__item promo__item--<?= $value['class']; ?>">
+                <a class="promo__link" href="pages/all-lots.html"><?= $value['name']; ?></a>
+            </li>
         <?php endforeach; ?>
+
     </ul>
 </section>
 <section class="lots">
@@ -16,25 +17,26 @@
     </div>
     <ul class="lots__list">
         <!--список из массива с товарами-->
-        <?php foreach ($lots_info as $key => $item): ?>
-        <li class="lots__item lot">
-            <div class="lot__image">
-                <img src="<?= $item['url_img']; ?>" width="350" height="260" alt="">
-            </div>
-            <div class="lot__info">
-                <span class="lot__category"><?= $item['category']; ?></span>
-                <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= htmlspecialchars($item['title']); ?></a></h3>
-                <div class="lot__state">
-                    <div class="lot__rate">
-                        <span class="lot__amount">Стартовая цена</span>
-                        <span class="lot__cost"><?= get_formatted_amount($item['price']); ?></span>
-                    </div>
-                    <div class="lot__timer timer <?php if ($time_end <= 3600): ?>timer--finishing<?php endif; ?>">
-                        <?= gmdate('H:i', $time_end); ?>
+        <?php foreach ($lots_info as $key => $item) : ?>
+            <li class="lots__item lot">
+                <div class="lot__image">
+                    <img src="<?= $item['url_img']; ?>" width="350" height="260" alt="">
+                </div>
+                <div class="lot__info">
+                    <span class="lot__category"><?= $item['category']; ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= htmlspecialchars($item['title']); ?></a></h3>
+                    <div class="lot__state">
+                        <div class="lot__rate">
+                            <span class="lot__amount">Стартовая цена</span>
+                            <span class="lot__cost"><?= get_formatted_amount($item['price']); ?></span>
+                        </div>
+                        <div class="lot__timer timer <?php if ($time_end <= $time) : ?>timer--finishing<?php endif; ?>">
+                            <?= gmdate('H:i', $time_end); ?>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </li>
+            </li>
         <?php endforeach; ?>
+
     </ul>
 </section>
